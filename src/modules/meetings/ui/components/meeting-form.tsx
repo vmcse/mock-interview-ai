f840784049b4cf4bb3e7f55328daa40cd463c3d3
Vamsi Madhav PV <vmcse09@gmail.com>
2025-06-29 02:28:20 +0530

    Create meetings form

---
+++ src/modules/meetings/ui/components/meeting-form.tsx
@@ -0,0 +1,184 @@
+import { z } from "zod";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { meetingsInsertSchema } from "../../schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { MeetingGetOne } from "../../types";
+import { useState } from "react";
+import { CommandSelect } from "@/components/command-select";
+import { Divide } from "lucide-react";
+import { CreateAgentDialog } from "@/modules/agents/ui/components/agents-create-dialog";
+
+interface Props {
+  onSuccess?: (id?: string) => void;
+  onCancel?: () => void;
+  initialValues?: MeetingGetOne;
+}
+
+export const MeetingForm = ({ onSuccess, onCancel, initialValues }: Props) => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  const [openNewAgentDialog, setOpenNewAgentDialog] = useState(false);
+  const [agentSearch, setAgentSearch] = useState("");
+
+  const agents = useQuery(
+    trpc.agents.getMany.queryOptions({
+      pageSize: 100,
+      search: agentSearch,
+    })
+  );
+
+  const createMeeting = useMutation(
+    trpc.meetings.create.mutationOptions({
+      onSuccess: async (data) => {
+        await queryClient.invalidateQueries(
+          trpc.meetings.getMany.queryOptions({})
+        );
+
+        onSuccess?.(data.id);
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    })
+  );
+
+  const updateMeeting = useMutation(
+    trpc.meetings.update.mutationOptions({
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(
+          trpc.meetings.getMany.queryOptions({})
+        );
+
+        if (initialValues?.id) {
+          await queryClient.invalidateQueries(
+            trpc.agents.getOne.queryOptions({ id: initialValues.id })
+          );
+        }
+        onSuccess?.();
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    })
+  );
+
+  const form = useForm<z.infer<typeof meetingsInsertSchema>>({
+    resolver: zodResolver(meetingsInsertSchema),
+    defaultValues: {
+      name: initialValues?.name ?? "",
+      agentId: initialValues?.agentId ?? "",
+    },
+  });
+
+  const isEdit = !!initialValues?.id;
+  const isPending = createMeeting.isPending || updateMeeting.isPending;
+
+  const onSubmit = (values: z.infer<typeof meetingsInsertSchema>) => {
+    if (isEdit) {
+      updateMeeting.mutate({ ...values, id: initialValues.id });
+    } else {
+      createMeeting.mutate(values);
+    }
+  };
+
+  return (
+    <>
+      <CreateAgentDialog
+        open={openNewAgentDialog}
+        onOpenChange={setOpenNewAgentDialog}
+      />
+      <Form {...form}>
+        <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            name="name"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="e.g. Math consultation" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            name="agentId"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Agent</FormLabel>
+                <FormControl>
+                  <CommandSelect
+                    options={(agents.data?.items ?? []).map((agent) => ({
+                      id: agent.id,
+                      value: agent.id,
+                      children: (
+                        <div className="flex items-center gap-x-2">
+                          <GeneratedAvatar
+                            seed={agent.name}
+                            variant="botttsNeutral"
+                            className="border size-6"
+                          />
+                          <span>{agent.name}</span>
+                        </div>
+                      ),
+                    }))}
+                    onSelect={field.onChange}
+                    onSearch={setAgentSearch}
+                    value={field.value}
+                    placeholder="Select an agent"
+                  />
+                </FormControl>
+                <FormDescription>
+                  Would you like to create a new agent?{" "}
+                  <button
+                    type="button"
+                    className="text-primary hover:underline"
+                    onClick={() => setOpenNewAgentDialog(true)}
+                  >
+                    Click here
+                  </button>
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex justify-between gap-x-2">
+            {onCancel && (
+              <Button
+                variant="ghost"
+                disabled={isPending}
+                type="button"
+                onClick={() => onCancel()}
+              >
+                Cancel
+              </Button>
+            )}
+            <Button disabled={isPending} type="submit">
+              {isEdit ? "Update" : "Create"}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </>
+  );
+};
---
+++ src/modules/meetings/ui/components/meetings-create-dialog.tsx
@@ -0,0 +1,29 @@
+import { ResponsiveDialog } from "@/components/responsive-dialog";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { MeetingForm } from "./meeting-form";
+
+interface Props {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const CreateMeetingDialog = ({ open, onOpenChange }: Props) => {
+  const router = useRouter();
+  return (
+    <ResponsiveDialog
+      title="New Meeting"
+      description="Create a new meeting"
+      open={open}
+      onOpenChange={onOpenChange}
+    >
+      <MeetingForm
+        onSuccess={(id) => {
+          onOpenChange(false);
+          router.push(`meetings/${id}`);
+        }}
+        onCancel={() => onOpenChange}
+      />
+    </ResponsiveDialog>
+  );
+};
